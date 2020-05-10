fbb13f4f1e6587ead672eb43d51092ebfe741053
Jörg Reinhardt <joerg.reinhardt@cloud.ionos.com>
2020-05-10 19:40:13 +0200

    REFACTOR
      - Documentation Improved

--- category.go
+++ category.go
@@ -1,6 +1,32 @@
 /*
  CATEGORY OBJECT INTERFACE AND UNIT TYPES
 
+Clearup Category
+
+Category is cluttered by dispatch of lable/id/flag accessors.
+Category will only be accessed by index via 'Obj.Id() -> int'.
+  ⇒ category constructor needs to derive Kind from int Id:
+    ⇒ kind needs to be kind[0]
+      ⇒ kind needs to return all []kind
+        ← in order to access sibling kinds
+      ⇒ kind needs to return its kind: kind[0].Kind()
+        ← in order to walk tree for generation of uuid
+
+  '''
+  Category  ∷ C  →  Obj…
+
+    id   C  = id    C →  int		   |  pos in parent category
+    card C  = card  C →  int		   |  length
+    name C  = name  C →  string		   |  name of category (flag lable, or symbol)
+					   |
+    cons    = C  →  C₀			   |  cons empty root category
+    cons O… = O… →  (O.Id()… → Cₙ) → Cₙ₊ₘ  |  cons 'cat from cat & objects
+					   |
+    kind    = C  →  Cₙ…			   |  kind ∅ returns all kinds	    → C…
+    kind Cₙ = Cₙ →  (Cₙ → C₀) → Cₚₐᵣₑₙₜ	   |  kind C returns kind <of> O(C) → Cₚ
+    kind Oₙ = Oₙ →  (id Oₙ → Cₙ) → C₍ₕᵢₗ₎  |  kind O returns kind <of> O₍   → Cₒ
+  '''
+
  every thing is an object and needs to implement the object interface 'Obj'.
  that includes internal parts of the type system, like type markers and
  names.  the interface demands a 'Type() int' method to return a unique
